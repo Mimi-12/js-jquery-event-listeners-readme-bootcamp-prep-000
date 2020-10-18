45c3fc22b84a861f6fd8731091b3efbafc5c52a6
selam lemma <oneiconforlife26@gmail.com>
2020-10-18 04:47:46 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,13 @@
 //define functions here
 
+function getIt(){
+
+}
+
 $(document).ready(function(){
-
+$("p").click(function()){
+  $("p").click("Hey");
+)}
 // call functions here
-
+return $("p")
 });
